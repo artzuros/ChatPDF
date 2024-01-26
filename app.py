3463331436348fcb0fb6762d035d0ae791f13f92
artzuros <gunubansal129@gmail.com>
2024-01-26 18:40:01 +0530

    updated api key location

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def get_pdf_text(pdf_docs):
     text=""
